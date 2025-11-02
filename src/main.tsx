b34c3446e3fee9c7ebc6711afd5455afe55912a0
Matthew Reardon <patriotnewsactivism@proton.me>
2025-11-02 09:24:19 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './auth/AuthProvider';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Why: App depends on AuthProvider; neither imports App back.
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
